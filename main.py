280cfff72c69fedc6c856f484da5d70d6d503a9e
clem <clementine.mirande@gmail.com>
2024-02-19 22:59:58 +0100

    commentaires vite fait

--- main.py
+++ main.py
@@ -3,26 +3,27 @@ from tkinter import *
 formule = ""
 
 
-def click(num):
+def clique(num):
     global formule
     formule = formule + str(num)
     equation.set(formule)
 
 
-def equalclick():
+def egale_clique():
     try:
         global formule
-
-        result = str(eval(formule))
-        equation.set(result)
-        formule = result
+        # eval permet de d'éxécuter la "formule" en python
+        # par exemple 3+2 sera éxécuter comme si on l'avait écrit directement en dur
+        resultat = str(eval(formule))
+        equation.set(resultat)
+        formule = resultat
 
     except:
-        equation.set(" error ")
+        equation.set(" erreur ")
         formule = ""
 
 
-def effacer():
+def efface():
     global formule
     formule = ""
     equation.set("")
@@ -33,33 +34,34 @@ if __name__ == "__main__":
     master.title("Calculatrice")
     master.geometry("460x315")
     equation = StringVar()
+
     formule_field = Entry(master, textvariable=equation)
     formule_field.grid(columnspan=3, pady=30, padx=20, ipadx=100, ipady=10)
-    btn_1 = Button(master, text=' 1 ', command=lambda: click(1), height=2, width=10)
+
+    # lambda pour les boutons permet de retarder l'appel de clique
+    # jusqu'a ce que le bouton soit cliqué
+    btn_1 = Button(master, text=' 1 ', command=lambda: clique(1), height=2, width=10)
     btn_1.grid(row=2, column=0)
 
-    btn_2 = Button(master, text=' 2 ', command=lambda: click(2), height=2, width=10)
+    btn_2 = Button(master, text=' 2 ', command=lambda: clique(2), height=2, width=10)
     btn_2.grid(row=2, column=1)
 
-    btn_3 = Button(master, text=' 3 ', command=lambda: click(3), height=2, width=10)
+    btn_3 = Button(master, text=' 3 ', command=lambda: clique(3), height=2, width=10)
     btn_3.grid(row=3, column=0)
 
-    btn_4 = Button(master, text=' 4 ', command=lambda: click(4), height=2, width=10)
+    btn_4 = Button(master, text=' 4 ', command=lambda: clique(4), height=2, width=10)
     btn_4.grid(row=3, column=1)
 
-
-    plus = Button(master, text=' + ', command=lambda: click("+"), height=2, width=10)
+    plus = Button(master, text=' + ', command=lambda: clique("+"), height=2, width=10)
     plus.grid(row=2, column=2)
 
-    minus = Button(master, text=' - ', command=lambda: click("-"), height=2, width=10)
-    minus.grid(row=3, column=2)
+    moins = Button(master, text=' - ', command=lambda: clique("-"), height=2, width=10)
+    moins.grid(row=3, column=2)
 
+    egale = Button(master, text=' = ', command=egale_clique, height=2, width=10)
+    egale.grid(row=5, column=2)
 
-    equal = Button(master, text=' = ', command=equalclick, height=2, width=10)
-    equal.grid(row=5, column=2)
-
-    effacer = Button(master, text='effacer', command=effacer, height=2, width=10)
+    effacer = Button(master, text='effacer', command=efface, height=2, width=10)
     effacer.grid(row=6, column=2)
-
 
     master.mainloop()
