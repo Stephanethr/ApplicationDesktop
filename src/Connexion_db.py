317328fb11b9c4f40627961be8fb239051517c6e
Stephanethr <stethiry9@gmail.com>
2024-03-04 12:03:32 +0100

    Connexion_db est devenu une classe

--- src/Connexion_db.py
+++ src/Connexion_db.py
@@ -1,45 +1,58 @@
 import os
 import sqlite3
 from src.requetes_sql import *
-from src.chiffrement import *
-
-def connect_db():
-    # Construire le chemin absolu vers la base de données
-    database_folder = "../database"
-    database_path_file = os.path.join(database_folder, "passGuardian.db")
-
-    # Vérifier si le dossier database existe, sinon le créer
-    if not os.path.exists(database_folder):
-        os.makedirs(database_folder)
+from src.Chiffrement import *
 
 
-    # Vérifier si la base de données existe sinon la créer
-    if not os.path.exists(database_path_file):
-        # Si la base de données n'existe pas, créer la base de données et la structure de table
-        conn = sqlite3.connect(database_path_file)
-        cursor = conn.cursor()
+class ConnexionDB:
+    def __init__(self):
+        self.conn = None
+        self.cursor = None
 
-        # Créer la base de données
-        create_db(cursor)
+    def get_cursor(self):
+        return self.cursor
 
-        conn.commit()
-        conn.close()
+    def get_conn(self):
+        return self.conn
 
-    # Connexion à la base de données
-    conn = sqlite3.connect(database_path_file)
-    cursor = conn.cursor()
-    return conn, cursor
+    def connect_db(self):
+        # Construire le chemin vers la base de données
+        database_folder = "../database"
+        database_path_file = os.path.join(database_folder, "passGuardian.db")
 
-def close_db(conn):
-    # Fermer la connexion à la base de données
-    conn.close()
+        # Vérifier si le dossier database existe, sinon le créer
+        if not os.path.exists(database_folder):
+            os.makedirs(database_folder)
+
+        # Vérifier si la base de données existe sinon la créer
+        if not os.path.exists(database_path_file):
+            # Si la base de données n'existe pas, créer la base de données et la structure de table
+            self.conn = sqlite3.connect(database_path_file)
+            self.cursor = self.conn.cursor()
+
+            # Créer la base de données
+            create_db(self.cursor)
+
+            self.conn.commit()
+            self.conn.close()
+
+        # Connexion à la base de données
+        self.conn = sqlite3.connect(database_path_file)
+        self.cursor = self.conn.cursor()
+
+    def commit(self):
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
+
 
 if __name__ == "__main__":
-    conn, cursor = connect_db()
-    create_user(cursor, "admin", encrypt_password("admin"))
-    save_password(cursor, encrypt_password("password"), "category", "site", 1)
-    conn.commit()
-    close_db(conn)
+    chiffrement = Chiffrement()
+    connDB = ConnexionDB()
+    connDB.connect_db()
 
-
-
+    create_user(connDB.get_cursor(), "admin", chiffrement.encrypt_password("admin"))
+    save_password(connDB.get_cursor(), chiffrement.encrypt_password("password"), "category", "site", 1)
+    connDB.commit()
+    connDB.close()
