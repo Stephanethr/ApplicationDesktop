3854f1f21b741c497b813b47291aa9a3e64d0916
clem <clementine.mirande@gmail.com>
2024-03-11 14:53:30 +0100

    correction fautes d'orthographe

--- src/Generateur_mdp.py
+++ src/Generateur_mdp.py
@@ -4,10 +4,10 @@ import random
 
 
 class Generation:
-    LONGEUR_MIN_MDP = 12
-    LONGEUR_MAX_MDP = 21
-    LONGEUR_MIN_CHOIX = 1
-    LONGEUR_MAX_CHOIX = 4
+    LONGUEUR_MIN_MDP = 12
+    LONGUEUR_MAX_MDP = 21
+    LONGUEUR_MIN_CHOIX = 1
+    LONGUEUR_MAX_CHOIX = 4
     LTR_MIN_MIN_MDP = 3
     LTR_MAJ_MIN_MDP = 3
     CHIFFRE_MIN_MDP = 3
@@ -17,7 +17,7 @@ class Generation:
     lettre_maj = ''
     chiffres = ''
     symboles = ''
-    longeur_mdp = 0
+    longueur_mdp = 0
     alphabet = ''
     nbr_chiffre_obligatoire = 0
     nbr_symbole_obligatoire = 0
@@ -30,59 +30,59 @@ class Generation:
         self.chiffres = string.digits
         self.symboles = string.punctuation
 
-    def longeur_valide(self, longeur_mdp: int) -> bool:
+    def longueur_valide(self, longueur_mdp: int) -> bool:
         """
-            Teste la validité de la longeur du mot de passe choisit
-            entrer par l'utilisateur
+            Teste la validité de la longueur du mot de passe choisie
+            entrée par l'utilisateur
 
-            Test si la longeur du mot de passe est bien comprise
-            entre 12 et 21. Si oui assigne le nombre entré par
-            l'utilisateur à la varible de la classe.
-            Sinon retourn False
+            Teste si la longueur du mot de passe est bien comprise
+            entre 12 et 21. Si oui, assigne le nombre entré par
+            l'utilisateur à la variable de la classe.
+            Sinon, retourne False
 
-            :param longeur_mdp: nombre entrer par l'utilisateur
-            :type longeur_mdp: int
+            :param longueur_mdp: nombre entré par l'utilisateur
+            :type longueur_mdp: int
             :return: Choix valide ou non
             :rtype: bool
         """
         valide = True
-        if longeur_mdp < self.LONGEUR_MIN_MDP or longeur_mdp > self.LONGEUR_MAX_MDP:
-            print("La longeur du mot de passe n'est pas valide")
+        if longueur_mdp < self.LONGUEUR_MIN_MDP or longueur_mdp > self.LONGUEUR_MAX_MDP:
+            print("La longueur du mot de passe n'est pas valide")
             valide = False
         else:
-            self.longeur_mdp = longeur_mdp
+            self.longueur_mdp = longueur_mdp
         return valide
 
     def choix_alphabet_valide(self, choix: list[int]) -> bool:
         """
-            Teste la validité des choix entrer par l'utilisateur
+            Teste la validité des choix entrés par l'utilisateur
 
             Teste en premier la taille de la liste des choix,
             s'il y a plus de 4 choix alors il y a une erreur dans la liste
-            soit un choix est supérieur à 4 soit un choix est entrer
+            soit un choix est supérieur à 4 soit un choix est entré
             plusieurs fois dans la liste.
             Ensuite, on teste la validité des choix dans la liste
             si le nombre est compris entre 1 et 4 alors le choix est valide.
 
-            :param choix: Liste entrer par l'utilisateur
+            :param choix: Liste entrée par l'utilisateur
             :type choix: list[int]
             :return: Choix valide ou non
             :rtype: bool
         """
         valide = True
-        if len(choix) < self.LONGEUR_MIN_CHOIX or len(choix) > self.LONGEUR_MAX_CHOIX:
-            print("Il y as trop de paramètre choisit. Le max est de 4")
+        if len(choix) < self.LONGUEUR_MIN_CHOIX or len(choix) > self.LONGUEUR_MAX_CHOIX:
+            print("Il y a trop de paramètres choisis. Le maximum est de 4")
             valide = False
 
         for i in choix:
-            if not self.LONGEUR_MIN_CHOIX <= i <= self.LONGEUR_MAX_CHOIX:
-                print("Une des valeurs entré n'est pas valide (choix de 1 à 4)")
+            if not self.LONGUEUR_MIN_CHOIX <= i <= self.LONGUEUR_MAX_CHOIX:
+                print("Une des valeurs entrées n'est pas valide (choix de 1 à 4)")
                 valide = False
         return valide
 
-    def choix_utilisateur(self, charactere_voulue: list[int]) -> bool:
+    def choix_utilisateur(self, caractere_voulu: list[int]) -> bool:
         """
-            Crée un alphabet personnalisé selon les choix entré par l'utilisateur.
+            Crée un alphabet personnalisé selon les choix entrés par l'utilisateur.
 
             Prend en paramètre une liste d'entiers représentant les types de caractères
             à inclure dans l'alphabet souhaité. Les entiers sont définis comme suit dans le dictionnaire
@@ -96,8 +96,8 @@ class Generation:
             Par exemple, pour créer un alphabet contenant des lettres minuscules et des chiffres,
             l'utilisateur doit fournir la liste [1, 3].
 
-            :param: charactere_voulue: Liste d'entiers représentant les types de caractères à inclure.
-            :type: charactere_voulue: list[int]
+            :param: caractere_voulu: Liste d'entiers représentant les types de caractères à inclure.
+            :type: caractere_voulu: list[int]
             :rtype: bool
         """
         conditions = {
@@ -109,9 +109,9 @@ class Generation:
 
         alphabet = ''
         valide = True
-        if self.choix_alphabet_valide(charactere_voulue):
-            for condition in charactere_voulue:
-                alphabet = alphabet + conditions[condition]
+        if self.choix_alphabet_valide(caractere_voulu):
+            for condition in caractere_voulu:
+                alphabet += conditions[condition]
             self.alphabet = alphabet
 
             if any(lettre_min in self.lettre_min for lettre_min in self.alphabet):
@@ -137,7 +137,7 @@ class Generation:
 
             :param mot_de_passe: mot de passe généré
             :type mot_de_passe: str
-            :return: Choix valide ou non
+            :return: Mot de passe avec les caractères obligatoires ajoutés
             :rtype: str
         """
 
@@ -157,13 +157,13 @@ class Generation:
 
     def generation_mdp(self, nbr_mdp: int) -> list[str]:
         """
-                Génére le nombre de mot de passe voulue en fonction des
-                conditions rentrer au préalable par l'utilisateur (1 : lettre min, 2: lettre maj,...)
+        Génère le nombre de mots de passe voulus en fonction des
+        conditions entrées au préalable par l'utilisateur (1 : lettre min, 2: lettre maj,...)
 
-                :param nbr_mdp: le nombre de mot de passe à générer
-                :type nbr_mdp: int
-                :return: Liste de mots de passe généré
-                :rtype: list[str]
+        :param nbr_mdp: le nombre de mots de passe à générer
+        :type nbr_mdp: int
+        :return: Liste de mots de passe générés
+        :rtype: list[str]
         """
 
         mots_de_passe = []
@@ -171,10 +171,10 @@ class Generation:
         for i in range(nbr_mdp):
             mot_de_passe = ''
 
-            longeur_mdp = (self.longeur_mdp - self.nbr_chiffre_obligatoire - self.nbr_symbole_obligatoire -
-                           self.nbr_lettre_min_obligatoire - self.nbr_lettre_maj_obligatoire)
+            longueur_mdp = (self.longueur_mdp - self.nbr_chiffre_obligatoire - self.nbr_symbole_obligatoire -
+                            self.nbr_lettre_min_obligatoire - self.nbr_lettre_maj_obligatoire)
 
-            for j in range(longeur_mdp):
+            for j in range(longueur_mdp):
                 mot_de_passe += secrets.choice(self.alphabet)
 
             mot_de_passe = self.mot_de_passe_valide(mot_de_passe)
@@ -187,5 +187,3 @@ class Generation:
             mots_de_passe.append(mot_de_passe)
 
         return mots_de_passe
-
-
