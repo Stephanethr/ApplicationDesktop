6cbee4231ab8d7a81f29fd8128d0989e64dc16d6
Matthieu Michelozzi <michelozzimatthieu@gmail.com>
2024-03-12 15:54:21 +0100

    ajout commentaires

--- PassController.py
+++ PassController.py
@@ -11,7 +11,20 @@ class PassController:
         self.chiffrement = None
 
     def generate_password(self, answers):
-        # enregistre les entrées de l'utilisateur dans une liste
+        """
+            Cette méthode génère un mot de passe en fonction des réponses de l'utilisateur.
+
+            Les réponses de l'utilisateur sont stockées dans une liste. Ensuite, la méthode vérifie si la longueur du mot de passe
+            et les options de mot de passe fournies par l'utilisateur sont valides. Si elles sont valides, un mot de passe est généré
+            et renvoyé.
+
+            Args:
+                answers (dict): Un dictionnaire contenant les réponses de l'utilisateur. Il doit contenir les clés 'password_length',
+                'password_options' et 'password_count'.
+
+            Returns:
+                str: Le mot de passe généré si les entrées de l'utilisateur sont valides. Sinon, None est renvoyé.
+        """
         self.user_inputs = answers
         if (self.generation.longueur_valide(self.user_inputs['password_length']) &
                 self.generation.choix_utilisateur(self.user_inputs['password_options'])):
@@ -24,6 +37,14 @@ class PassController:
 
     def create_user(self, username, password):
         # déplacer conn dans requetes_sql
+        """
+            Cette méthode crée un nouvel utilisateur dans la base de données.
+
+            Args:
+                username (str): Le nom d'utilisateur pour le nouvel utilisateur.
+                password (str): Le mot de passe pour le nouvel utilisateur.
+
+        """
         conn = Connexion_db.ConnexionDB()
         conn.connect_db()
         requetes_sql.create_user_bdd(conn.cursor, username, password)
@@ -31,6 +52,19 @@ class PassController:
         conn.close()
 
     def get_user(self, username, password, key):
+        """
+            Cette méthode récupère un utilisateur de la base de données.
+
+            Elle crée d'abord un chiffrement avec la clé fourni et ensuite un nouvel utilisateur.
+
+            Args:
+                username (str): Le nom d'utilisateur de l'utilisateur à récupérer.
+                password (str): Le mot de passe de l'utilisateur à récupérer.
+                key (str): La clé utilisée pour créer le chiffrement.
+
+            Returns:
+                User: L'utilisateur récupéré de la base de données, ou None si aucun utilisateur ne correspond aux identifiants fournis.
+            """
         self.createChiffrement(key)
         conn = Connexion_db.ConnexionDB()
         conn.connect_db()
@@ -39,6 +73,19 @@ class PassController:
         return user
 
     def save_password(self, login, password, categoryName, siteName, userID):
+        """
+                Cette fonction chiffre le mot de passe fourni à l'aide de l'objet chiffrement de la classe
+                actuelle.
+
+            Args:
+                self: L'instance de la classe actuelle.
+                login (str): Le nom d'utilisateur associé au mot de passe.
+                password (str): Le mot de passe non chiffré à enregistrer.
+                categoryName (str): Le nom de la catégorie à laquelle le mot de passe appartient.
+                siteName (str): Le nom du site ou du service associé au mot de passe.
+                userID (str): L'identifiant de l'utilisateur auquel le mot de passe est associé.
+
+        """
         cipher = self.chiffrement.encrypt_password(password)
         conn = Connexion_db.ConnexionDB()
         conn.connect_db()
@@ -47,6 +94,17 @@ class PassController:
         conn.close()
 
     def get_passwords(self, userID):
+        """
+            Cette méthode récupère les mots de passe d'un utilisateur spécifique de la base de données.
+            Les mots de passe récupérés sont déchiffrés avant d'être renvoyés.
+
+            Args:
+                userID (int): L'ID de l'utilisateur pour lequel récupérer les mots de passe.
+
+            Returns:
+                list: Une liste de tuples, chaque tuple contenant les détails d'un mot de passe. Les détails incluent l'ID du mot de passe,
+                le nom du site, le mot de passe déchiffré, le nom d'utilisateur et l'URL du site.
+        """
         conn = Connexion_db.ConnexionDB()
         conn.connect_db()
         res = requetes_sql.get_passwords(conn.cursor, userID)
@@ -58,6 +116,12 @@ class PassController:
         return res
 
     def delete_password(self, password_id):
+        """
+            Cette méthode supprime un mot de passe spécifique de la base de données.
+
+            Args:
+                password_id (int): L'ID du mot de passe à supprimer.
+        """
         conn = Connexion_db.ConnexionDB()
         conn.connect_db()
         requetes_sql.delete_password(conn.cursor, password_id)
@@ -65,4 +129,12 @@ class PassController:
         conn.close()
 
     def createChiffrement(self, password):
+        """
+            Cette méthode crée un objet de chiffrement avec le mot de passe fourni.
+
+            L'objet de chiffrement est utilisé pour chiffrer et déchiffrer les données.
+
+            Args:
+                password (str): Le mot de passe utilisé pour créer l'objet de chiffrement.
+        """
         self.chiffrement = Chiffrement.Chiffrement(password)
