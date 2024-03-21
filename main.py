bbb6301e156a206f173e1b88b70dbb58511a0343
Stephanethr <stethiry9@gmail.com>
2024-03-21 15:13:04 +0100

    Ajout detection theme systeme dark ou light pour adapter l'application

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 import pyperclip
 from Controller import Controller
 import hashlib
+import subprocess
 
 
 class Main:
@@ -14,6 +15,41 @@ class Main:
         self.root = tk.Tk()
         self.root.title("PassGuardian")
         self.create_connect_menu()
+
+
+    def check_os_theme(self):
+        """Checks DARK/LIGHT mode of Windows."""
+        try:
+            import winreg
+
+            registry = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+            reg_keypath = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize'
+            try:
+                reg_key = winreg.OpenKey(registry, reg_keypath)
+            except FileNotFoundError:
+                return "light"
+            for i in range(1024):
+                try:
+                    value_name, value, _ = winreg.EnumValue(reg_key, i)
+                    if value_name == 'AppsUseLightTheme':
+                        if value == 0:
+                            return "dark"
+                except OSError:
+                    break
+            return "light"
+
+        except ImportError:
+            """Checks DARK/LIGHT mode of macos."""
+            cmd = 'defaults read -g AppleInterfaceStyle'
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE, shell=True)
+
+            check = bool(p.communicate()[0])
+            if check:
+                return "dark"
+            else:
+                return "light"
+
 
     def create_connect_menu(self):
         self.connect_frame = ttk.Frame(self.root, padding="20")
@@ -266,6 +302,7 @@ class Main:
         self.root.destroy()
 
     def run(self):
+        print(self.check_os_theme())
         self.root.mainloop()
 
 
